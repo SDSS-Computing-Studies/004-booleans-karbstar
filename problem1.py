d44a9cb8c4bdfde2e27ad134ef4b6eaabda05028
karbstar <90715867+karbstar@users.noreply.github.com>
2021-09-27 14:35:21 -0700

    c

--- problem1.py
+++ problem1.py
@@ -14,6 +14,6 @@
 
 num = int(input("enter a number."))
 if (num % 2) ==0:
-    print(f"{num} is a even number") 
+    print(f"{0} is a even number") 
 else:
-    print(f"{num} is a odd number")
+    print(f"{0} is a odd number")
